eb01e9b7d6b5f4eacc4be9723fbcfe83f7459436
SauryANever23 <sauryajha9841@gmail.com>
2025-06-09 15:56:45 +0545

    getting closer to nowhere

--- mario.c
+++ mario.c
@@ -2,9 +2,21 @@
 
 void build(int height)
 {
-  for (int i = 0; i < height; i++)
+  for (int i = 1; i <= height; i++)
   {
     for (int j = 0; j < i; j++)
+    {
+      printf("#");
+    }
+    printf("\n");
+  }
+}
+
+void revrse_build(int height)
+{
+  for (int i = 1; i <= height; i++)
+  {
+    for (int j = height; j >= i; j--)
     {
       printf("#");
     }
@@ -19,6 +31,8 @@ int main(void)
   scanf("%d", &height);
 
   build(height);
+  printf("\n");
+  revrse_build(height);
 
   return 0;
 }
