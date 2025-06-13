e53082356e503953169520299aaf96a3659a83b8
SauryANever23 <sauryajha9841@gmail.com>
2025-06-13 21:08:09 +0545

    making functions for solving the problem related to list functions

---
+++ hackerrank/list_commands.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    N = int(input())
+    
+    lst = []
+
+    def ins(e, i):
+        lst.insert(e, i)
+    
+    def prt(lst):
+        print(lst)
+
+    def rm(e):
+        lst.remove(e)
+
+    def
+
