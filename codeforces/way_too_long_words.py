c183f06eddbc6fa2c7fa65faf8a81ff6c08ddd3b
SauryANever23 <sauryajha9841@gmail.com>
2025-11-30 16:52:20 +0545

    solving problems way_too_long_words

---
+++ codeforces/way_too_long_words.py
@@ -0,0 +1,20 @@
+# way to long words: if len > 10 
+# abbreviation format: first lennter<between: number of letters between them: int>last letter
+
+n = int(input())
+
+def abbrev(word: str):
+    fl, ll = word[0], word[-1]
+    num = len(word) - 2 
+    return fl+str(num)+ll
+
+word_arr = []
+for _ in range(n):
+    word = input()
+    word_arr.append(word)
+
+for i in range(len(word_arr)):
+    if len(word_arr[i]) <= 10:
+        print(word_arr[i])
+    else:
+        print(abbrev(word_arr[i]))
