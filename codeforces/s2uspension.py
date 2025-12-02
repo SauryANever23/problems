d4cb80daf8c890516bcd928c05715072295d7394
SauryANever23 <sauryajha9841@gmail.com>
2025-12-02 17:22:45 +0545

    suspension final

--- codeforces/s2uspension.py
+++ codeforces/s2uspension.py
@@ -7,15 +7,23 @@ for _ in range(t):
     n = int(input())
     cards = list(map(int, input().split()))
     num = 0 
-    num += cards[1]
-    if num < n and cards[0] < n:
-        if (cards[0] % 2) == 0:
-            num += cards[0]/2
-        else:
-            num += (cards[0]-1)/2
+    num = cards[1] + (cards[0]//2)
     
-    suspension.append(num)
+    suspension.append(min(num, n))
 
 for i in suspension:
     print(int(i))
+
+"""
+alternative method:
+
+y, r = map(int, input().split())
+
+elms_by_r = r 
+elems_by_y = y//2 
+
+total_elms = r + y//2 
+
+suspension.append(min(total_elims, n))
+"""
 
