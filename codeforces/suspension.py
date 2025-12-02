f7478838aa4b937f100c836c8d62687f38762b59
SauryANever23 <sauryajha9841@gmail.com>
2025-12-02 14:58:31 +0545

    suspension problem, codeforces contest 1067 div 2

---
+++ codeforces/suspension.py
@@ -0,0 +1,24 @@
+# a game being played 
+# initially n players 
+# yellow cards: y, and red cards: r (for fouls)
+# two ways pllayers can be suspended
+# --> 2 y or 1 red 
+# once suspended, out of game, n-- 
+# determine the maximum number of people suspended from game 
+
+# for y and r 
+# 
+
+t = int(input())
+num=0
+for _ in range(t):
+    n = int(input())
+    for i in range(n):
+        cards = list(map(int, input().split()))
+        num += cards[1]
+        if cards[0] % 2 == 0:
+            num += cards[0]/2 
+        else:
+            cards[0] -= 1 
+            num += cards[0]/2
+print(num)
