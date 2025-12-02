4284239fe8d2f29ff83f9fc89dcc11e689c20647
SauryANever23 <sauryajha9841@gmail.com>
2025-12-02 18:29:35 +0545

    for a >= width, problem solved

--- codeforces/theater_square.py
+++ codeforces/theater_square.py
@@ -4,15 +4,19 @@
 # how many flagstones required to fill the square? 
 
 n, m, a = map(int, input().split())
-
+p = 0 
 def small(a, b):
     if a > b:
-        return b 
+        return b
+    elif a == b:
+        return a
     else:
         return a 
 def big(a, b):
     if a > b:
         return a 
+    elif a ==b:
+        return a
     else:
         return b
 
@@ -21,8 +25,13 @@ width = small(m, n)
 lenght = big(m, n)
 
 if (a >= width):
-    p = lenght // a 
-print(p)
+    if lenght % a == 0:
+        p = lenght / a 
+    else:
+        p = (lenght // a) + 1
+elif (a < width):
+    pass
+print(int(p))
 
 
 
