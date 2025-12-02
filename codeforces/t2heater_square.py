031a61154faf731fe1e7ff0c0a160c66c9e7a6cf
SauryANever23 <sauryajha9841@gmail.com>
2025-12-02 19:43:36 +0545

    runtime error

--- codeforces/t2heater_square.py
+++ codeforces/t2heater_square.py
@@ -2,12 +2,13 @@
 # formula for ceilling 
 # as the problem is two dimentional, 
 # calculate the tiles required in both m and n dimentions 
+import math 
 
 n, m, a = map(int, input())
 
 # ceilling division 
-lenght_flagstones = (n+a-1)//a 
-width_flagstones = (m+a-1)//a 
+lenght_flagstones = math.ceil(n/a)
+width_flagstones = math.ceil(m/a)
 
 print(lenght_flagstones*width_flagstones)
 
