10b25eabd0a893a587aa93a8639dfe3083736cef
SauryANever23 <sauryajha9841@gmail.com>
2025-12-02 17:03:33 +0545

    trying a greedy algorythm

--- codeforces/s2uspension.py
+++ codeforces/s2uspension.py
@@ -1,2 +1,20 @@
 # suspension greedy 
 
+t = int(input())
+
+suspension=[]
+for _ in range(t):
+    n = int(input())
+    cards = list(map(int, input().split()))
+    num = 0 
+    num += cards[1]
+    if num < n:
+        if (cards[0] % 2) == 0:
+            num += cards[0]/2
+        else:
+            num -= (cards[0]-1)/2
+    suspension.append(num)
+
+for i in suspension:
+    print(int(i))
+
