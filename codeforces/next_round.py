814a0d916e308d8187d3c5565f3dc1a00f37e082
SauryANever23 <sauryajha9841@gmail.com>
2025-12-03 20:25:53 +0545

    next round add

--- codeforces/next_round.py
+++ codeforces/next_round.py
@@ -1,10 +1,13 @@
 # next round 
 
 n, k = map(int, input().split())
-
+print(n, k)
 scores = list(map(int, input().split()))
 
 count = 0
 for i in scores:
-    print(i)
+    if i > k:
+        print(i,k,i>=k)
+        count+=1 
+
 print(count)
