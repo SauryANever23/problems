4bd145ff73e3ccbbaf17aaf0e4c2ec8c4e8f0604
SauryANever23 <sauryajha9841@gmail.com>
2025-12-03 22:33:01 +0545

    min value for the b values

--- codeforces/min_absolute_sum.py
+++ codeforces/min_absolute_sum.py
@@ -6,7 +6,12 @@ for i in range(t):
     n=int(input())
     a = list(map(int, input().split()))
     b = []
+    min=0
     for i in range(n-1):
         obj = a[i+1]-a[i]
         b.append(obj)
-    print(a, "\n", b)
+    for i in b:
+        min += i
+    min = abs(min)
+
+
