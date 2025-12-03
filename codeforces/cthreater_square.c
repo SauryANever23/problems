812afc87895eb9ce4ec2753ad5a96ecc54985df9
SauryANever23 <sauryajha9841@gmail.com>
2025-12-03 17:01:47 +0545

    no extra functions needed

--- codeforces/cthreater_square.c
+++ codeforces/cthreater_square.c
@@ -1,22 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int min(int a, int b)
-{
-  if (a>b)
-  {
-    return a;
-  }
-  else 
-  {
-    return b
-  }
-}
-
 int main(void)
 {
   int m,n,a;
   scanf("%d %d %d", &m, &n, &a);
-  
-  printf("%d", min(total, a))
+  int lenght = (m+a-1)/a; 
+  int width = (n+a-1)/a;
+
+  int total = lenght * width;
+  printf("%d", total);
+
+  return 0;
 }
