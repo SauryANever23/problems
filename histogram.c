d9f4d29cccd9056fc1d879fd858287a26a70f29a
SauryANever23 <sauryajha9841@gmail.com>
2026-01-09 18:34:58 +0545

    initializing functions

--- histogram.c
+++ histogram.c
@@ -1,6 +1,18 @@
+/*
+*Write a program to print a histogram of the lengths of words in its input. It is
+easy to draw the histogram with the bars horizontal;
+*/
+
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX 200 
+
+int word_separate(char sentence[MAX]);
+void histogram(char *word); 
 
 int main(void)
 {
-
+  
 }
