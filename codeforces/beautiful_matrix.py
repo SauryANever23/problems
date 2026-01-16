b2097f999881e8bffc968adac2ac725624de4839
SauryANever23 <sauryajha9841@gmail.com>
2026-01-16 17:59:32 +0545

    getting the index values

--- codeforces/beautiful_matrix.py
+++ codeforces/beautiful_matrix.py
@@ -14,4 +14,11 @@ for _ in range(5):
     row = list(map(int, input().split()))
     arr.append(row) 
 
-print(arr)
+arr_indx, num_index = 0,0
+for i in range(5):
+    for j in range(5):
+        if arr[i][j] == 1: 
+            arr_indx = i 
+            num_indx = j 
+
+
