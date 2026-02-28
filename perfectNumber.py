2ea4b462ca8df0241c2e3cf6aa177c4b8e09752a
SauryANever23 <sauryajha9841@gmail.com>
2026-02-28 09:09:36 +0545

    solving the problem untill 10000 numbers in 2.5 seconds

--- perfectNumber.py
+++ perfectNumber.py
@@ -1,21 +1,25 @@
 """
 Trying to find perfect numbers
 """
+import time 
 
 def is_perfect(n: int) -> bool:
     div_array = []
-    for i in range(n):
+    for i in range(1, n):
         if n % i == 0:
             div_array.append(i)
-    div_array.remove(n)
     sm = sum(div_array)
     if sm == n:
         return True 
 
 def main():
+    star_time = time.perf_counter()
     for i in range(10000):
         if is_perfect(i):
             print(i)
+    end_time = time.perf_counter()
+    time_elapsed = end_time - star_time
+    print(f"Elapsed Time: {time_elapsed:0.4f} seconds")
 
 if __name__ == '__main__':
     main()
