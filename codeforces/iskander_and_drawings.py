ca884b4193a87ff72e264566edf3b6700883d43d
SauryANever23 <sauryajha9841@gmail.com>
2026-07-14 21:26:19 +0545

    iskander and drawing first test case passed

--- codeforces/iskander_and_drawings.py
+++ codeforces/iskander_and_drawings.py
@@ -14,16 +14,26 @@ for i in range(t):
     lines = list(pages[i])
     clean_lines = [l for l in lines if l != "*"]
     if '*' not in pages[i]:
-        soln.append(n//2)
+        soln.append(int(size[i]/2))
     elif len(clean_lines) == 0: 
         soln.append(0)
+    elif pages[i].startswith('*') or pages[i].endswith('*'): 
+        clean = pages[i]
+        soln.append(len(clean)//2)
     else:
         count = 0 
         break_lines = pages[i].split('*')
-        for index in range(len(break_lines)): 
-            if len(break_line[index]) in [1, 2]:
+
+        for index in range(len(break_lines)//2): 
+            if len(break_lines[index]) in [1, 2]:
                 count += 1 
-            else: 
+                
+            
+        soln.append(count)
+                
+for sol in soln:
+    print(sol)
+
                 
 
 
