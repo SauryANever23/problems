4d2e2e4be9065dc7e7389054c00882587c1963e9
SauryANever23 <sauryajha9841@gmail.com>
2026-08-24 18:17:56 +0545

    cses problem no 3:repititions

---
+++ cses/repetitions.cpp
@@ -0,0 +1,74 @@
+/* problem: https://cses.fi/problemset/task/1069 
+ * date: 24/08/2026
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Type Aliases for faster typing
+using ll = long long;
+using lli = long long int;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+
+// Shortcuts for Containers & Loops
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define sz(a) int((a).size())
+
+// Repetitive Loop Macros
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define per(i, a, b) for (int i = a; i >= b; --i)
+
+// Constants
+const int INF = 1e9 + 7;
+const ll LINF = 1e18 + 7;
+const int MOD = 1e9 + 7; // or 998244353
+
+// Core logic for a single testcase
+void solve() {
+   string s; 
+   cin >> s; 
+
+   long long current = 1; 
+   long long mx = 1; 
+
+   for (int i = 0; i < s.size()-1; i++)
+   {
+     if (s[i] == s[i+1])
+     {
+       current += 1; 
+     }
+     else {
+       current = 1; 
+     }
+
+     mx=max(current, mx);
+   }
+   cout << mx << "\n";
+   
+}
+
+int main() {
+    // Optimize standard I/O operations for performance
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t = 1;
+    // cin >> t; // Comment this out if the problem has only 1 test case
+    
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
+
+
